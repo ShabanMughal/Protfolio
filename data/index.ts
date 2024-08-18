a26a2656bdd124e50d85693ed3570025941a42ce
ShabanMughal <user.thandermughal@gmail.com>
2024-08-18 17:21:02 +0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,190 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "I am currently seeking a position as a Frontend Developer",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to know about me?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Admin Dashboard",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/sass.svg", "/vit.svg"],
+    link: "https://dashboard-khaki-six-34.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Popular Gancha Game - Clone",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.png",
+    iconLists: ["/vit.svg", "/tail.svg", "/re.svg", "/fm.svg"],
+    link: "https://pgr-game.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "UI/UX Design - Clone",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "vit.svg", "/tail.svg", "/fm.svg"],
+    link: "https://brainwave-red.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/vit.svg", "/tail.svg", "/re.svg", "/three.svg", "/gsap.svg"],
+    link: "https://iphone-one-iota.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Three.js is a powerful JavaScript library that enables developers to create sophisticated 3D graphics and animations directly within the web browser. Built on top of WebGL, Three.js abstracts the complexities of low-level graphics programming, providing an intuitive and accessible API for building 3D scenes.",
+    name: "Three JS",
+    title: "What is three.js",
+    img: "/three.svg",
+  },
+  {
+    quote:
+      "React is a popular JavaScript library developed by Facebook for building user interfaces, particularly for single-page applications. It allows developers to create reusable UI components, which can manage their own state and be composed to form complex user interfaces. React's virtual DOM efficiently updates and resulting in improved performance. ",
+    name: "React JS",
+    title: "What is react.js",
+    img: "re.svg",
+  },
+  {
+    quote:
+      "Tailwind CSS is a utility-first CSS framework designed to provide developers with a highly customizable and efficient way to style web applications. Unlike traditional CSS frameworks that offer predefined components, Tailwind CSS focuses on utility classes that can be composed to build any design directly in the markup.",
+    name: "Tailwind css",
+    title: "What is tailwind css",
+    img: "/tail.svg",
+  },
+  {
+    quote:
+      "Next.js is a powerful React framework developed by Vercel that enables developers to build high-performance web applications with ease. It offers a hybrid static and server-side rendering approach, allowing developers to pre-render pages at build time or request time. This results in faster load times and improved SEO, making Next.js an excellent choice.",
+    name: "Next JS",
+    title: "What is next.js",
+    img: "/next.svg",
+  },
+  {
+    quote:
+      "GSAP's modular structure allows developers to include only the necessary parts of the library, optimizing load times and performance. With its robust documentation and active community, GSAP has become an indispensable tool for creating engaging and dynamic user interfaces in modern web development.",
+    name: "GreenSock Animation Platform",
+    title: "What is the gsap ",
+    img: "/gsap.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "UX/UI Designs",
+    desc: "Designed and developed web apps and mobile apps with platforms using figma.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Video editor",
+    desc: "A skilled video editor adept at transforming raw footage into compelling visual stories.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "3d vistual Developer",
+    desc: "An aspiring 3D developer actively learning to create immersive and interactive 3D experiences",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/ShabanMughal",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/thunder_senpai_22?utm_source=qr&igsh=MTExa3g3FoaDhmcg==",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/thunder-mughal-27386a2a9?utm_source=share_via&utm_content=profile&utm_medium=
